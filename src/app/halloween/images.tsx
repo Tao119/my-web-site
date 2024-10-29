fcd8d94a3db3dfdf2a7babe65cac1bdedfb21659
Tao119 <tao.dama.art@gmail.com>
2024-10-29 20:08:33 +0900

    a

--- src/app/halloween/images.tsx
+++ src/app/halloween/images.tsx
@@ -3,7 +3,7 @@ import image1 from "src/assets/img/kks.jpg";
 import image2 from "src/assets/img/kd.jpg";
 import image3 from "src/assets/img/s.jpg";
 import image4 from "src/assets/img/t.jpg";
-import image5 from "src/assets/img/k.jpg";
+import image5 from "src/assets/img/kn.jpg";
 import image6 from "src/assets/img/kt.jpg";
 import image7 from "src/assets/img/r.jpg";
 
